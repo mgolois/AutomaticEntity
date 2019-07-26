03b0ed033e16ade871c027d571eb894627638bbd
Golois Mouelet <mgolois@yahoo.com>
2019-07-26 10:02:26 -0400

    updated test class name

--- AutomaticEntity.Tests/DbOperationTest.cs
+++ AutomaticEntity.Tests/DbOperationTest.cs
@@ -4,15 +4,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AutomaticEntity.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class DbOperationTest
     {
         private DbOperation dbOperation;
-        public UnitTest1()
+        public DbOperationTest()
         {
+            //TODO Add support for in memory DB
             dbOperation = new DbOperation("Data Source=.;Initial Catalog=School;Integrated Security=True");
         }
         [TestMethod]
-        public void TestMethod1()
+        public void CanGetEntity()
         {
             var student = dbOperation.GetEntity<Students>(4);
             Assert.IsNotNull(student);
